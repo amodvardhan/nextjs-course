be4dd642034d2a8c5e3148ca10a227915ba508c8
amodvardhan <amod.vardhan002@gmail.com>
2021-04-11 15:13:36 +0530

    navigations and component

--- event-app/pages/index.js
+++ event-app/pages/index.js
@@ -1,7 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
+
 function HomePage() {
+  const featureEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featureEvents} />
     </div>
   );
 }
